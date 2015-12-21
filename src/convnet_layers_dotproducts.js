6bb7d80c834cf66c9c1ecbac4c4bc59afbe194fb
Ryosuke Okuta <okuta@preferred.jp>
2015-12-21 14:49:30 +0900

    Implement deconv

--- src/convnet_layers_dotproducts.js
+++ src/convnet_layers_dotproducts.js
@@ -7,6 +7,7 @@
   // schemes: 
   // - FullyConn is fully connected dot products 
   // - ConvLayer does convolutions (so weight sharing spatially)
+  // - DeconvLayer does deconvolutions (so weight sharing spatially)
   // putting them together in one file because they are very similar
   var ConvLayer = function(opt) {
     var opt = opt || {};
@@ -170,6 +171,142 @@
     }
   }
 
+  var get_deconv_outsize = function(size, k, s, p) {
+    return s * (size - 1) + k - 2 * p;
+  }
+
+  var DeconvLayer = function(opt) {
+    var opt = opt || {};
+
+    // required
+    this.out_depth = opt.filters;
+    this.sx = opt.sx; // filter size. Should be odd if possible, it's cleaner.
+    this.in_depth = opt.in_depth;
+    this.in_sx = opt.in_sx;
+    this.in_sy = opt.in_sy;
+
+    // optional
+    this.sy = typeof opt.sy !== 'undefined' ? opt.sy : this.sx;
+    this.stride = typeof opt.stride !== 'undefined' ? opt.stride : 1; // stride at which we apply filters to input volume
+    this.pad = typeof opt.pad !== 'undefined' ? opt.pad : 0; // amount of 0 padding to add around borders of input volume
+    this.l1_decay_mul = typeof opt.l1_decay_mul !== 'undefined' ? opt.l1_decay_mul : 0.0;
+    this.l2_decay_mul = typeof opt.l2_decay_mul !== 'undefined' ? opt.l2_decay_mul : 1.0;
+
+    // computed
+    // note we are doing floor, so if the strided convolution of the filter doesnt fit into the input
+    // volume exactly, the output volume will be trimmed and not contain the (incomplete) computed
+    // final application.
+    if(typeof opt.out_sx !== 'undefined') {
+      this.out_sx = opt.out_sx;
+    } else {
+      this.out_sx = get_deconv_outsize(this.in_sx, this.sx, this.stride, this.pad);
+    }
+    if(typeof opt.out_sy !== 'undefined') {
+      this.out_sy = opt.out_sy;
+    } else {
+      this.out_sy = get_deconv_outsize(this.in_sy, this.sy, this.stride, this.pad);
+    }
+    this.layer_type = 'deconv';
+
+    // initializations
+    var bias = typeof opt.bias_pref !== 'undefined' ? opt.bias_pref : 0.0;
+    this.filters = [];
+    for(var i=0;i<this.out_depth;i++) { this.filters.push(new Vol(this.sx, this.sy, this.in_depth)); }
+    this.biases = new Vol(1, 1, this.out_depth, bias);
+  }
+  DeconvLayer.prototype = {
+    forward: function(V, is_training) {
+      // optimized code by @mdda that achieves 2x speedup over previous version
+
+      this.in_act = V;
+      var A = new Vol(this.out_sx |0, this.out_sy |0, this.out_depth |0, 0.0);
+
+      var V_sx = V.sx |0;
+      var V_sy = V.sy |0;
+      var xy_stride = this.stride |0;
+
+      for(var d=0;d<this.out_depth;d++) {
+        var f = this.filters[d];
+        for(var ay=0; ay<this.in_sy; y++,ay++) {  // xy_stride
+          var y = (ay * xy_stride - this.pad)|0;
+          for(var ax=0; ax<this.in_sx; x+=xy_stride,ax++) {  // xy_stride
+            var x = (ax * xy_stride - this.pad)|0;
+            // convolve centered at this particular location
+            for(var fy=0;fy<f.sy;fy++) {
+              var iy = y+fy; // coordinates in the original input array coordinates
+              for(var fx=0;fx<f.sx;fx++) {
+                var ix = x+fx;
+                if(iy>=0 && iy<V_sy && ix>=0 && ix<V_sx) {
+                  for(var fd=0;fd<f.depth;fd++) {
+                    var a = f.w[((f.sx * fy)+fx)*f.depth+fd] * V.w[((V_sx * ay)+ax)*V.depth+fd];
+                    A[((f.sx * iy)+ix)*A.depth + f] += a;
+                  }
+                }
+              }
+            }
+          }
+        }
+        for(var ay=0; ay<this.out_sy; y++,ay++) {  // xy_stride
+          for(var ax=0; ax<this.out_sx; x++,ax++) {  // xy_stride
+            A[((f.sx * ay)+ax)*A.depth] += this.biases.w[d];
+          }
+        }
+      }
+    },
+    backward: function() {
+      // Not implement.
+    },
+    getParamsAndGrads: function() {
+      var response = [];
+      for(var i=0;i<this.out_depth;i++) {
+        response.push({params: this.filters[i].w, grads: this.filters[i].dw, l2_decay_mul: this.l2_decay_mul, l1_decay_mul: this.l1_decay_mul});
+      }
+      response.push({params: this.biases.w, grads: this.biases.dw, l1_decay_mul: 0.0, l2_decay_mul: 0.0});
+      return response;
+    },
+    toJSON: function() {
+      var json = {};
+      json.sx = this.sx; // filter size in x, y dims
+      json.sy = this.sy;
+      json.stride = this.stride;
+      json.in_depth = this.in_depth;
+      json.out_depth = this.out_depth;
+      json.out_sx = this.out_sx;
+      json.out_sy = this.out_sy;
+      json.layer_type = this.layer_type;
+      json.l1_decay_mul = this.l1_decay_mul;
+      json.l2_decay_mul = this.l2_decay_mul;
+      json.pad = this.pad;
+      json.filters = [];
+      for(var i=0;i<this.filters.length;i++) {
+        json.filters.push(this.filters[i].toJSON());
+      }
+      json.biases = this.biases.toJSON();
+      return json;
+    },
+    fromJSON: function(json) {
+      this.out_depth = json.out_depth;
+      this.out_sx = json.out_sx;
+      this.out_sy = json.out_sy;
+      this.layer_type = json.layer_type;
+      this.sx = json.sx; // filter size in x, y dims
+      this.sy = json.sy;
+      this.stride = json.stride;
+      this.in_depth = json.in_depth; // depth of input volume
+      this.filters = [];
+      this.l1_decay_mul = typeof json.l1_decay_mul !== 'undefined' ? json.l1_decay_mul : 1.0;
+      this.l2_decay_mul = typeof json.l2_decay_mul !== 'undefined' ? json.l2_decay_mul : 1.0;
+      this.pad = typeof json.pad !== 'undefined' ? json.pad : 0;
+      for(var i=0;i<json.filters.length;i++) {
+        var v = new Vol(0,0,0,0);
+        v.fromJSON(json.filters[i]);
+        this.filters.push(v);
+      }
+      this.biases = new Vol(0,0,0,0);
+      this.biases.fromJSON(json.biases);
+    }
+  }
+
   var FullyConnLayer = function(opt) {
     var opt = opt || {};
 
@@ -270,6 +407,7 @@
   }
 
   global.ConvLayer = ConvLayer;
+  global.DeconvLayer = DeconvLayer;
   global.FullyConnLayer = FullyConnLayer;
   
 })(convnetjs);
